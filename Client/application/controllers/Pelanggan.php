3dda969c31b339b0dd69c95e4a14e50fc15f3e0a
dediirawanuti <dedi_irawan@teknokrat.ac.id>
2022-03-22 20:21:50 +0700

    add simple_get

--- Client/application/controllers/Pelanggan.php
+++ Client/application/controllers/Pelanggan.php
@@ -6,6 +6,25 @@ class Pelanggan extends CI_Controller {
 	
 	public function index()
 	{
-		$this->load->view('pelanggan_tampil');
+		// Panggil helper "url (unutk direct halaman)"
+		$this->load->helper(array("url"));
+		
+		// panggil library "client (untuk baca "service_get" dari server)"
+		$this->load->library(array("client"));
+
+
+		// Tampilkan data "pelanggan" dari "service_get" server
+		$data["pelanggan"] = json_decode($this->client->simple_get(API_PELANGGAN));
+
+		// foreach ($data["pelanggan"] as $dt) {
+		// 	echo $dt->kode."<br>";
+		// 	echo $dt->nama."<br>";
+		// 	echo $dt->email."<br>";
+		// 	echo $dt->username."<br>";
+		// 	echo $dt->password."<br>";
+
+		// }
+
+		$this->load->view('pelanggan_tampil', $data);
 	}
 }
